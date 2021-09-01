e0c3a730a9509bc85e55fb61f2087b7fc5e8abc1
GAkers12 <goakers42@tntech.edu>
2021-09-01 10:47:25 -0500

    Sept. 1 Commit

--- Profile Example/js/lib/app.js
+++ Profile Example/js/lib/app.js
@@ -1,3 +1,6 @@
-$('#btntest').click(function() {
-    alert('Test');
+$('#btnTest').click(function() {
+    const decTaxRate = .0925;
+    let decHours = $('#txtHours').val();
+    let decRate = $('#txtPayRate').val();
+    console.log(decHours * decRate);
 });
